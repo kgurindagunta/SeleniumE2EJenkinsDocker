2fdd5c00097044c4b728e809977d101dbf653119
Kiran <kgurindagunta@salesforce.com>
2026-03-09 19:18:33 +0530

    WebDriver change

--- src/main/java/com/webDriver/Driver.java
+++ src/main/java/com/webDriver/Driver.java
@@ -3,7 +3,9 @@ package com.webDriver;
 import java.util.Objects;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public final class Driver {
 	
@@ -16,12 +18,20 @@ public final class Driver {
 
 		if(browser.equalsIgnoreCase("chrome")) {
 			if(Objects.isNull(DriverManager.getDriver())) {
-				DriverManager.setDriver(new ChromeDriver());
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("--headless=new");   // REQUIRED
+				options.addArguments("--no-sandbox");
+				options.addArguments("--disable-dev-shm-usage");
+				options.addArguments("--window-size=1920,1080");
+
+				DriverManager.setDriver(new ChromeDriver(options));
 			}
 		}
 		else if(browser.equalsIgnoreCase("firefox")) {
 			if(Objects.isNull(DriverManager.getDriver())) {
-				DriverManager.setDriver(new FirefoxDriver());
+				FirefoxOptions options = new FirefoxOptions();
+				options.addArguments("-headless");
+				DriverManager.setDriver(new FirefoxDriver(options));
 			}
 		}
 	}
